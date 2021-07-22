7a6765cb398e0b407ded29037a11b5b74c7c4b2b
aminbnd <2212@holbertonschool.com>
2021-07-22 13:42:37 +0200

    'Push'

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module that containes "canUnlockAll" function"""
+
+def canUnlockAll(boxes):
+    """ function
+    Args:
+        boxes: list of lists of integers
+    return: boolean: true or false
+    """
+    listOfKeys = [0]
+    for key in listOfKeys:
+        for k in boxes[key]:
+            if k not in listOfKeys:
+                listOfKeys.append(k)
+    for i in range(len(boxes)):
+        if i not in listOfKeys:
+            return False
+    return True
