1ae3d440faa087970a12fcc91353efc4deb04242
aminbnd <2212@holbertonschool.com>
2021-12-31 23:23:16 +0100

    Starwars characters

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request(url, async (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+  response = JSON.parse(body);
+  // console.log(response);
+  for (const charId of response.characters) {
+    await new Promise((resolve, reject) => {
+      request(charId, (error, response, body) => {
+        if (error) {
+          console.log(error);
+        }
+        const formatedResponse = JSON.parse(body);
+        console.log(formatedResponse.name);
+        resolve();
+      });
+    });
+  }
+});
