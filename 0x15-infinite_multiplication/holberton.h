e8176e6c72da7ddb5a5ce88d1a42baa30c2fa86c
aminbnd <2212@holbertonschool.com>
2022-01-15 16:44:15 +0100

    Push

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,16 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/* Headers */
+#include <stdlib.h>
+#include <stdbool.h>
+
+/* functions */
+int _putchar(char c);
+int _strlen(char *str);
+void _print_error(void);
+int _isDigit(char c);
+void _mul(char *nb, char *nb2, int nblength, int nb2length);
+
+
+#endif /* HOBERTON_H */
