489f13f2a7806b811f57edd924686a148b15dcc6
aminbnd <2212@holbertonschool.com>
2022-01-18 16:40:55 +0100

    Task

---
+++ 0x18-merge_sort/sort.h
@@ -0,0 +1,13 @@
+#ifndef SORT_H
+#define SORT_H
+
+/** Headers **/
+#include <stdlib.h>
+#include <stdio.h>
+
+/** Prototypes */
+void print_array(const int *array, size_t size);
+void merge_sort(int *array, size_t size);
+
+
+#endif /* SORT_H */
