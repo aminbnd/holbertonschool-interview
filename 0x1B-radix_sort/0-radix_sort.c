22523d1769d7bac1222bff128ce5aa4ece06d19c
aminbnd <2212@holbertonschool.com>
2022-05-17 08:55:28 +0100

    Solution

---
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -0,0 +1,58 @@
+#include "sort.h"
+
+/**
+ * getMax - Get the Max object
+ * 
+ * @arr: array to sort
+ * @n: integer 
+ * Return: int 
+ */
+
+int getMax(int arr[], int n)
+{
+	int max = arr[0];
+	int i;
+	for (i = 1; i < n; i++)
+		if(arr[i] > max)
+			max = arr[i];
+	return max;
+}
+
+/**
+ * countSort -  Funtiion
+ * 
+ * @arr: array of int 
+ * @n: int  
+ * @ex: intp 
+ */
+
+void countSort(int arr[], int n, int exp)
+{
+	int output[n];
+	int i, count[10] = {0};
+
+	for(i = 0; i < n; i++)
+		count[(arr[i] / exp) % 10]++;
+	for(i = 1; i < 10; i++)
+		count[i] += count[i - 1];
+
+	for( i = n - 1; i >= 0; i--)
+	{
+		output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+		count[(arr[i] / exp) % 10]--;
+	}
+
+	for(i = 0; i < n; i++)
+		arr[i] = output[i];
+}
+
+void radix_sort(int *array, size_t size)
+{
+	int m = getMax(array, size);
+	int exp;
+	for(exp = 1; m / exp > 0; exp *= 10)
+	{
+		countSort(array, size, exp);
+		print_array(array, size);
+	}
+}
