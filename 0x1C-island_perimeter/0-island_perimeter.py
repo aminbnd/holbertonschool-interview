db56862b25765498754e01c2ecce53af51d5fd12
aminbnd <2212@holbertonschool.com>
2022-05-17 10:27:02 +0100

    Fix solution

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -4,24 +4,18 @@
 
 def island_perimeter(grid):
     """ Function that returns the perimeter of an Island"""
+
     perim = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][i] == 1:
-                if(j+1) < len(grid[0]) and grid[i][j+1] == 0:
-                    perim += 1
-                if j == len(grid[0]) - 1:
-                    perim += 1
-                if grid[i][j-1] == 0 and j > 0:
-                    perim += 1
-                if j == 0:
-                    perim += 1
-                if grid[i-1][j] == 0 and i > 0:
-                    perim += 1
-                if i == 0:
-                    perim += 1
-                if (i+1) < len(grid) and grid[i+1][j] == 0:
-                    perim += 1
-                if i == len(grid) - 1:
-                    perim += 1
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                perim += 4
+                if row > 0 and grid[row - 1][col]:
+                    perim -= 1
+                if row < (len(grid) - 1) and grid[row + 1][col]:
+                    perim -= 1
+                if col > 0 and grid[row][col - 1]:
+                    perim -= 1
+                if col < (len(grid[row]) - 1) and grid[row][col + 1]:
+                    perim -= 1
     return perim
