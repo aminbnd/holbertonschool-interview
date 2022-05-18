772bbac5743238bddba64581f07c6f7a0bdc410f
aminbnd <2212@holbertonschool.com>
2022-05-18 18:47:55 +0100

    Header file

---
+++ 0x20-regex/regex.h
@@ -0,0 +1,14 @@
+#ifndef REGEX_H
+#define REGEX_H
+
+/*** Headers ***/
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+/*** Prototypes ***/
+int regex_match(char const *str, char const *pattern);
+
+
+#endif /* REGEX_H */
